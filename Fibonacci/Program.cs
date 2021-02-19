7dd46ae2e1c65088d7e8e139cb9b8919cea653f0
John <76816408+InterstellarRiz@users.noreply.github.com>
2021-02-18 20:13:41 -0500

    Fibonacci

---
+++ Fibonacci/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int value1 = 0;
+            int value2 = 1;
+
+            Console.WriteLine(value1);
+            Console.WriteLine(value2);
+
+            for (int i = 0; i < 18; i++)
+            {
+                int value3 = value1 + value2;
+                Console.WriteLine(value3);
+
+                value1 = value2;
+                value2 = value3;
+            }
+        }
+    }
+}
