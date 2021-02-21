3629e05ba43d1b1c3b8c19766d88f32809b131df
John <76816408+InterstellarRiz@users.noreply.github.com>
2021-02-21 16:05:05 -0500

    Shapes

---
+++ Shapes/Circle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    class Circle
+    {
+        private double radius = 1;
+
+        public Circle(double r)
+        {
+            radius = r;
+        }
+        public double GetArea() => Math.PI * (radius * radius);
+        public double GetCircumference() => 2 * Math.PI * radius;
+        public double GetRadius() => radius;
+        public void SetRadius(double radius)
+        {
+            if (radius < 0)
+            {
+                radius = -radius;
+            }
+            else if (radius == 0)
+            {
+                radius = 1;
+            }
+            this.radius = radius;
+        }
+    }
+}
