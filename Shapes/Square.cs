8d21d9fd2056a6353fdc47a875996f5abc948174
John <76816408+InterstellarRiz@users.noreply.github.com>
2021-02-21 16:06:07 -0500

    Shapes

---
+++ Shapes/Square.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes
+{
+    class Square
+    {
+        private double side = 1;
+        public Square(double side1)
+        {
+            side = side1;
+        }
+        public double GetArea() => 2 * side;
+        public double GetPerimeter() => side * 4;
+        public double GetSide() => side;
+        public void Setside(double side)
+        {
+
+            if (side < 0)
+            {
+                side = -side;
+            }
+            else if (side == 0)
+            {
+                side = 1;
+            }
+            this.side = side;
+        }
+
+    }
+}
