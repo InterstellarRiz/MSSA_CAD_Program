10d7e38922f805ebf802da9a1d525154bf3145b0
John <76816408+InterstellarRiz@users.noreply.github.com>
2021-02-21 17:17:52 -0500

    ReducingSteps

---
+++ ReducingSteps/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ReducingSteps
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NumberOfSteps(14);
+            NumberOfSteps(8);
+            NumberOfSteps(123);
+        }
+
+        static void NumberOfSteps(int num)
+        {
+            int stepNumbers = 0;
+            while (num != 0)
+            {
+                if (num % 2 == 0)
+                {
+                    num /= 2;
+                    stepNumbers++;
+                }
+                else
+                {
+                    num -= 1;
+                    stepNumbers++;
+                }
+            }
+            Console.WriteLine(stepNumbers);
+        }
+    }
+}
