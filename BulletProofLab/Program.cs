30725a8e3da18a3989983a4e69b6694321455c05
John <76816408+InterstellarRiz@users.noreply.github.com>
2021-02-21 17:24:54 -0500

    BulletProofLab

---
+++ BulletProofLab/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace LabCh._6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Bullet1();
+            Bullet2();
+        }
+
+        private static int Bullet1()
+        {
+            int userInput = 0;
+            bool commit = false;
+            while (commit == false)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter an integer: ");
+                    userInput = int.Parse(Console.ReadLine());
+                    Console.WriteLine($"Your input was: {userInput}");
+                    commit = true;
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("You did not use an integer!");
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return userInput;
+        }
+
+
+        private static double Bullet2()
+        {
+            double userInput = 0;
+            bool commit = false;
+            while (commit == false)
+            {
+                try
+                {
+                    Console.WriteLine("Please enter a double: ");
+                    userInput = double.Parse(Console.ReadLine());
+                    Console.WriteLine($"Your input was: {userInput}");
+                    commit = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("You did not use a double!");
+                    Console.WriteLine(e.Message);
+                }
+            }
+            return userInput;
+        }
+
+    }
+}
+
+
