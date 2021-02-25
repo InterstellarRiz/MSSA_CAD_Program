13d6f1af3de8fec0e2ad0a468d6508ed82ef0ffc
John <76816408+InterstellarRiz@users.noreply.github.com>
2021-02-24 23:29:22 -0500

    Add files via upload

---
+++ ParkingLot/ParkingLot.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingLot
+{
+    public class ParkingLot
+    {    // building fields to hold the type of cars
+        private double _numofLargespaces { get; set; }
+        private double _numofMediumspaces { get; set; }
+        private double _numofSmallspaces { get; set; }
+
+        // does this need to be a string or double ... someone is calling this constructor and inputing the number of spaces 
+        public ParkingLot(double numofLargespaces, double numofMediumspaces, double numofSmallspaces) // requires caller to speciffy how many of each kind of car the parking lot holds
+        {
+            _numofLargespaces = numofLargespaces;
+            _numofMediumspaces = numofMediumspaces;      // store the spaces in the fields
+            _numofSmallspaces = numofSmallspaces;
+        }
+        // they will call the method to try and put a car in the parking lot
+        public bool AddCar(string carType)
+        {
+            string _carType = carType.ToLower();
+            bool isThereSpace = true;
+
+            if (_carType == "large")
+            {
+                // I want to take the string from the user... look at the # of large spaces... if value is < spaces then yes there is room ... subtract value from space and so on
+                //
+                if (_numofLargespaces > 0)
+                {
+                    _numofLargespaces -= 1;
+                    return isThereSpace = true;
+                }
+                else
+                {
+                    return isThereSpace = false;
+                }
+            }
+            if (_carType == "medium")
+            {
+                // I want to take the string from the user... look at the # of large spaces... if value is < spaces then yes there is room ... subtract value from space and so on
+                //
+                if (_numofMediumspaces > 0)
+                {
+                    _numofMediumspaces -= 1;
+                    return isThereSpace = true;
+                }
+                else
+                {
+                    return isThereSpace = false;
+                }
+            }
+            if (_carType == "small")
+            {
+                // I want to take the string from the user... look at the # of large spaces... if value is < spaces then yes there is room ... subtract value from space and so on
+                //
+                if (_numofSmallspaces > 0)
+                {
+                    _numofSmallspaces -= 1;
+                    return isThereSpace = true;
+                }
+                else
+                {
+                    return isThereSpace = false;
+                }
+            }
+            return isThereSpace;
+        }
+        public double GetLargeSpace()
+        {
+            return _numofLargespaces;
+        }
+        public double GetMediumSpace()
+        {
+            return _numofMediumspaces;
+        }
+        public double GetSmallSpace()
+        {
+            return _numofSmallspaces;
+        }
+    }
+
+    
+}
---
+++ ParkingLot/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace ParkingLot
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Parking Lot!");
+            Console.WriteLine("This parking lot consist of spaces for : Large, Medium, and Small vehicles.");
+            ParkingSpaceQuestion();
+        }
+
+        public static void ParkingSpaceQuestion()
+        {
+            ParkingLot alpha = new ParkingLot(5, 5, 5);
+            bool loop = false;
+            while (loop == false)
+
+            {
+                Console.WriteLine("What type of car are you trying to park: ");
+                string _answer = Console.ReadLine();
+                string answer = _answer.ToLower();
+                if (answer == "large" || answer == "medium" || answer == "small")
+                {
+                    alpha.AddCar(answer);
+                    //Console.WriteLine("Anymore cars (yes or no)? ");
+                   // string _answer2 = Console.ReadLine();
+                   // string answer2 = _answer2.ToLower();
+                   // if (answer2 == "yes")
+                   // {
+                   //     ParkingSpaceQuestion();
+                   //     alpha.AddCar(answer);
+                   // }
+                   // else
+                   // {
+                   //     loop = true;
+                   // }
+                    loop = true;
+                }
+                else
+                {
+                    Console.WriteLine("You did not enter: Large, Medium, or Small \n Please try again");
+                    loop = false;
+                }
+            }
+            double largeParking;
+            double mediumParking;
+            double smallParking;
+
+            largeParking = alpha.GetLargeSpace();
+            mediumParking = alpha.GetMediumSpace();
+            smallParking = alpha.GetSmallSpace();
+
+            Console.WriteLine($"There are {largeParking} large parking spaces left.\n\t There are {mediumParking} medium parking spaces left.\n\t\t There are {smallParking} small parking spaces left.");
+        }
+    }
+}
