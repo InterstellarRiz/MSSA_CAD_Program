9db3a2cd5013ed272cf224c5cbec2bb3d9550297
John <76816408+JohnRizor@users.noreply.github.com>
2021-04-12 00:30:42 -0400

    Add files via upload

---
+++ WordGuessingGame/WordGuessingGame/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WordGuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random randomName = new Random();
+            Console.WriteLine("I am thinking of a person's name who is an athlete in today's sport.");
+        }
+    }
+}
